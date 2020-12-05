0474e5b55edeccaabbb165d133d5e269cfd6021a
Eric-Zhang-Projects <zhang.eric39@gmail.com>
2020-12-04 23:12:26 -0500

    updating about page

--- Web-App/frontend/src/components/login/about.jsx
+++ Web-App/frontend/src/components/login/about.jsx
@@ -6,22 +6,23 @@ export class About extends React.Component {
     render() {
         return <div>
         
-        <div className = "base-container" >
+        <div className = "base-container">
         <Navbar />
+            <div style={{"padding":"15vh"}}>
             To Do:
-            <p> Protected Routes(done)</p>
-            <p>Pages:</p>
-            <ul>
-            <li>filter by us vs country</li>
+            <hr/>
+            <ul style={{"": "left"}}>
                 <li>FIND OUT HOW TO DELETE CACHE ON LOGOUT</li>
                 <li>Toast/Alert user after setting new default city</li>
                 <li>Email notifications</li>
-                <br/>
+                <li> create lambda with cron job</li>
+                <hr/>
                 <li> forgot password</li>
                 <li> password encryption</li>
-                <li> create lambda with cron job</li>
+                <li>filter by us vs country</li>
+
             </ul>
-       
+            </div>       
         </div>
         </div>
         }
